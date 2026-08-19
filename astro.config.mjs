cc7e8a9b13122896e0d90679a0a0b83b925957ea
Jonathan Lewis <Jonathan.lewis@welch.edu>
2026-08-19 16:55:18 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from "@tailwindcss/vite";
+//npm install tailwindcss @tailwindcss/vite
+//npm install -D @tailwindcss/typography
+
+import sitemap from '@astrojs/sitemap';
+//npm run astro add sitemap
+
+
+
+export default defineConfig({
+  trailingSlash: 'always',
+
+  build: {
+    format: 'directory', // Ensures pages are built as /page/index.html
+  },
+
+  site: 'https://example.com',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  server: {
+    port: 3000,
+    open: true, 
+  },
+
+  integrations: [sitemap()],
+});
